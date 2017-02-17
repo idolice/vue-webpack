692ca0a37719776e87cb7a013965b466866ba251
idolice <349031587@qq.com>
2017-02-17 15:40:42 +0800

    .....

---
+++ my-project/src/List.vue
@@ -0,0 +1,8 @@
+<template>
+<div id="list">
+<ul>
+    <li><router-link to="/detail">1</a></ul>
+    <li><router-link to="/detail">2</a></ul>
+</ul>
+</div>
+</template>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import List from './List.vue'
+import Detail from './Detail.vue'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
+
+const routes = [
+  {path:'/detail', component:Detail}
+]
+
+const router = new VueRouter({routes:routes})
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#list',
+  render: h => h(List)
 })
+
+
